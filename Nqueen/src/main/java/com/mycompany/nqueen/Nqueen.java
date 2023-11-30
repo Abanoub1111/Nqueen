5a19d3b766c6c36c7b1c58c5baeefd1e5eb29484
Abanoub1111 <120176396+Abanoub1111@users.noreply.github.com>
2023-11-30 10:00:17 +0200

    Update Nqueen.java

--- Nqueen/src/main/java/com/mycompany/nqueen/Nqueen.java
+++ Nqueen/src/main/java/com/mycompany/nqueen/Nqueen.java
@@ -5,24 +5,24 @@ import java.util.ArrayList; // import the ArrayList class
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
-//import N_queens_solver_package.SolutionOBJ;
-
+import com.mycompany.nqueen.SolutionOBJ;
 
 public class Nqueen extends Thread {
+
     public static List<SolutionOBJ> solutions = Collections.synchronizedList(new ArrayList<SolutionOBJ>());
     boolean[][] board;
     int col;
+    int row;
     static int N;
     int TN;
-    
-    public Nqueen(boolean[][] board, int col, int N, int TN) {
-    this.board = board;
-    this.col = col;
-    this.N = N;
-    this.TN = TN;
+
+    public Nqueen(boolean[][] board, int row, int N, int TN) {
+        this.board = board;
+        this.row = row;
+        this.N = N;
+        this.TN = TN;
     }
-    
-    
+
     private static boolean isValid(boolean[][] board, int row, int col) { //already mfe4 7aga t7teh fa m4 lazm a check ta7teh
         for (int i = 0; i < row; i++) { //CHECK COLUMN
             if (board[i][col] == true) {
@@ -51,35 +51,31 @@ public class Nqueen extends Thread {
     }
 
     private static void theBt(boolean[][] board, int row, int TN) {
-    if (row == board.length) {
-        addSolution(new SolutionOBJ(board, TN));
-        return;
+        if (row == board.length) {
+            addSolution(new SolutionOBJ(board, TN));
+            return;
+        }
+
+        for (int i = 0; i < board.length; i++) {
+            if (isValid(board, row, i)) {
+                board[row][i] = true;
+                theBt(board, row + 1, TN);
+                board[row][i] = false;
+            }
+        }
     }
 
-    for (int i = 0; i < board.length; i++) {
-        if (isValid(board, row, i)) {
-            board[row][i] = true;
-            theBt(board, row + 1, TN);
-            board[row][i] = false;
-        }
-    }
-}
-    
     @Override
     public void run() {
-        theBt(board, col, TN);
+        theBt(board, row, TN);
     }
-    
+
     private static void addSolution(SolutionOBJ solution) {
-        
-        boolean[][] copiped_board = new boolean[N][N];
+        boolean[][] copiedBoard = new boolean[solution.board.length][solution.board.length];
         for (int i = 0; i < solution.board.length; i++) {
-            boolean[] aMatrix = solution.board[i];
-            int aLength = solution.board.length;
-            copiped_board[i] = new boolean[N];
-            System.arraycopy(aMatrix, 0, copiped_board[i], 0, aLength);
+            copiedBoard[i] = solution.board[i].clone();
         }
-        solutions.add(new SolutionOBJ(copiped_board, solution.TN));
+        solutions.add(new SolutionOBJ(copiedBoard, solution.TN));
     }
-   
+
 }
