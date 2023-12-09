1d3c3efe494994ebed05dbf501808bec3ccc4ecd
Basmala305 <hassanbasmala907@gmail.com>
2023-12-09 20:01:47 +0200

    nqueen

---
+++ Nqueen/src/main/java/com/mycompany/nqueen/Main.java
@@ -0,0 +1,87 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package nqueen;
+
+/**
+ *
+ * @author hp
+ */
+
+import java.util.* ;
+import nqueen.SolutionOBJ;
+import nqueen.Nqueen;
+
+public class Main {
+    static int N ;
+    public static void main(String[] args) {
+        try (Scanner input =new Scanner(System.in)) {
+            while (true){
+                System.out.println("Enter the number of Queens :");
+                N = input.nextInt();
+                if ( N == 2 || N ==3) {
+                    System.out.println("No Solution possible for "+ N +" Queens. Please enter another number");
+                }
+                else
+                    break;
+            }
+        }
+        ArrayList<Thread> ThArray = new ArrayList<Thread>();
+        int count = 0;
+        for (int row = 0; row < N; row++) {
+            for (int col = 0; col< N; col++) {
+                boolean board[][] = new boolean[N][N];
+                for (int i = 0; i < N; i++) {
+                    for (int j = 0; j < N;j++) {
+                        board[i][j] = false;
+                    }
+                }
+                board[row][col] = true;
+                Nqueen thread = new Nqueen(board, 1, N, count);
+                thread.start();
+                ThArray.add(thread);
+                count++;
+            }
+        }
+        try {
+            for (int i = 0; i < ThArray.size(); i++) {
+                ThArray.get(i).join();
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Thread interruption!.");
+        }
+       
+
+    }
+
+    public static void printSolution(SolutionOBJ solution) {
+        System.out.println( " Thread NO. " + solution.TN );
+        for (int row = 0; row < N; row++) {
+            System.out.print("║");
+            for (int col= 0; col < N; col++) {
+                if (col< N - 1) {
+                    if (solution.board[row][col] == true) {
+                        System.out.print('Q' + " ");
+                    } else {
+                        System.out.print('─' + " ");
+                    }
+                } else {
+                    if (solution.board[row][col] == true) {
+                        System.out.print('Q');
+                    } else {
+                        System.out.print('─');
+                    }
+                }
+            }
+            System.out.print("║");
+            System.out.println();
+        }
+
+    }
+}
+
+        
+        
+    
