2751aaf06c78aa1524bc47dcc7bfae3b7280981c
Basmala305 <hassanbasmala907@gmail.com>
2023-12-10 15:33:02 +0200

    nqueen

--- Nqueen/src/main/java/com/mycompany/nqueen/Main.java
+++ Nqueen/src/main/java/com/mycompany/nqueen/Main.java
@@ -1,14 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
+
 
 package nqueen;
 
-/**
- *
- * @author hp
- */
+
 
 import java.util.* ;
 import nqueen.SolutionOBJ;
