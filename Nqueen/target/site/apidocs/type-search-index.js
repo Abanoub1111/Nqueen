e3cc48ede7482a1535ce5fec493cf340cfacfc82
Abanoub1111 <120176396+Abanoub1111@users.noreply.github.com>
2023-12-17 00:25:21 +0200

    Delete Nqueen directory

--- Nqueen/target/site/apidocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.mycompany.nqueen","l":"Nqueen"},{"p":"com.mycompany.nqueen","l":"SolutionOBJ"}];updateSearchResults();
